a366b6f6c316383450f85b90a61a1b6e3f4eccd3
Randythegreat <randythegreat000@gmail.com>
2025-05-12 20:10:53 +0800

    yes

--- easypark-3/login2.php
+++ easypark-3/login2.php
@@ -4,7 +4,7 @@
 
         if ($_SERVER['REQUEST_METHOD'] == 'POST')
         {
-            if (isset($_POST['username']) && isset($_POST['password'])
+            if (isset($_POST['username']) && isset($_POST['password']))
             {
                 $username = $_POST['username'];
                 $password = $_POST['password'];
