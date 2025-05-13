880f3ea261fab8898fee7867e5cf31676f911be4
Randythegreat <randythegreat000@gmail.com>
2025-05-13 21:15:17 +0800

    footer

--- index.php
+++ index.php
@@ -225,43 +225,8 @@ if (isset($_SESSION['user_id'])) {
   </div>
 
 </div>
-<footer class="bg-dark text-light pt-5 pb-4">
-  <div class="container text-center text-md-left">
-    <div class="row">
-      <!-- Company Info -->
-      <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
-        <h5 class="text-uppercase mb-4 font-weight-bold text-warning">EASYPARK</h5>
-        <p>We help drivers find and reserve parking spots faster, smarter, and stress-free.</p>
-      </div>
 
-      <!-- Quick Links -->
-      <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
-        <h5 class="text-uppercase mb-4 font-weight-bold text-warning">Quick Links</h5>
-        <p><a href="#about" class="text-light" style="text-decoration: none;">About Us</a></p>
-        <p><a href="#terms" class="text-light" style="text-decoration: none;">Terms of Service</a></p>
-        <p><a href="#privacy" class="text-light" style="text-decoration: none;">Privacy Policy</a></p>
-        <p><a href="#contact" class="text-light" style="text-decoration: none;">Contact</a></p>
-      </div>
-
-      <!-- Social Links -->
-      <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3 text-center">
-        <h5 class="text-uppercase mb-4 font-weight-bold text-warning">Follow Us</h5>
-        <a href="https://www.facebook.com/randythegreat000" class="text-light mr-4" target="_blank" rel="noopener"><i class="fa fa-facebook fa-lg"></i></a>
-        <a href="https://x.com/AiruxPH" class="text-light mr-4" target="_blank" rel="noopener"><i class="fa fa-twitter fa-lg"></i></a>
-        <a href="https://www.instagram.com/itsmerandythegreat" class="text-light mr-4" target="_blank" rel="noopener"><i class="fa fa-instagram fa-lg"></i></a>
-        <a href="https://www.linkedin.com/in/anecito-randy-calunod-jr-326680210" class="text-light" target="_blank" rel="noopener"><i class="fa fa-linkedin fa-lg"></i></a>
-
-      </div>
-    </div>
-
-    <!-- Copyright -->
-    <div class="row mt-4">
-      <div class="col-md-12 text-center">
-        <p class="text-muted mb-0">&copy; 2025 EASYPARK. All Rights Reserved.</p>
-      </div>
-    </div>
-  </div>
-</footer>
+<?php include 'footer.php'; ?>
 
 <script src="js/jquery.min.js"></script>
 <script src="js/popper.min.js"></script>
