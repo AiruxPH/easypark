2f9a70d9747eff2a05a0ad9914aead96efd2063a
Randythegreat <randythegreat000@gmail.com>
2025-05-19 19:48:20 +0800

    sd

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -2,6 +2,6 @@
 
 <script src="../js/jquery.min.js"></script>
 <script src="../js/bootstrap.bundle.min.js"></script>
-<script src="js/ef9baa832e.js"></script>
+<script src="../js/ef9baa832e.js"></script>
 </body>
 </html>
