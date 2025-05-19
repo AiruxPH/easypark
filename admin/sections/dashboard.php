87a81da64d93150218666d07e853323d739d2141
Randythegreat <randythegreat000@gmail.com>
2025-05-19 20:17:38 +0800

    we

--- admin/sections/dashboard.php
+++ admin/sections/dashboard.php
@@ -91,15 +91,12 @@ $stats = [
     <div class="row mt-4">
         <div class="col-md-6 offset-md-3">
             <div class="card">
-                <div class="card-header">
-                    <h5>Recent Transactions</h5>
-                </div>
                 <div class="card-body">
                     <div class="table-responsive">
                         <table class="table table-striped">
                             <thead>
                                 <tr>
-                                    <th>ID</th>
+                                    <th>Payment ID</th>
                                     <th>Amount</th>
                                     <th>Method</th>
                                     <th>Status</th>
