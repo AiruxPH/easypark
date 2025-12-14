0adc29670b78fd9f4c021f5552812480bd7f3f36
AiruxPH <randythegreat000@gmail.com>
2025-12-15 00:02:59 +0800

    chore: delete unspecified files.

--- admin/admin-dashboard.php
+++
@@ -1,4 +0,0 @@
-<?php
-// Redirect legacy dashboard access to the main index controller
-header('Location: index.php');
-exit;
--- admin/transactions-section.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Transactions management section (placeholder)
-?>
-<div id="transactions-container" style="<?= isset($_GET['transactions']) ? '' : 'display:none;' ?>">
-  <!-- Transactions content goes here -->
-</div>
